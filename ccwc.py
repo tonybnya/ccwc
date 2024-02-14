86eb22b20d3b9996036b0445ce0ba218b4613dbc
Tony <nya.tony2010@gmail.com>
2024-02-14 12:04:02 +0100

    feat: add words counter function

--- ccwc.py
+++ ccwc.py
@@ -34,19 +34,19 @@ def ccwc_lines(filename: IO) -> int:
         lines = file.readlines()
 
     return len(lines)
-#
-#
-# def ccwc_words(filename: IO) -> int:
-#     """
-#     Count number of words
-#
-#     :param textfile: string containing the path to a text file
-#     :return: int representing the number of words
-#     """
-#     with open(filename) as file:
-#         words = file.read()
-#
-#     return len(words.split())
+
+
+def ccwc_words(filename: IO) -> int:
+    """
+    Count number of words
+
+    :param textfile: string containing the path to a text file
+    :return: int representing the number of words
+    """
+    with open(filename) as file:
+        words = file.read()
+
+    return len(words.split())
 #
 #
 # def ccwc_chars(filename: IO) -> int:
@@ -63,5 +63,5 @@ if __name__ == "__main__":
     args = sys.argv
     print(ccwc_bytes(args[1]))
     print(ccwc_lines(args[1]))
-    # print(ccwc_words(args[1]))
+    print(ccwc_words(args[1]))
     # print(ccwc_chars(args[1]))
