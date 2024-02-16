906f6cb9d2f7eaa5535b27f91999b949f04b78c4
Tony <nya.tony2010@gmail.com>
2024-02-16 09:39:51 +0100

    refactor: add count_print function to manage repetitive code for each flag handling

--- ccwc.py
+++ ccwc.py
@@ -3,6 +3,7 @@ ccwc - word, line, character, and byte count.
 
 Author: @tonybnya
 """
+from typing import Callable, List
 import argparse
 
 
@@ -10,8 +11,8 @@ def ccwc_bytes(filename: str) -> int:
     """
     Count number of bytes
 
-    :param textfile: the path to the text file
-    :return: the number of bytes in the file
+    :param filename: path to the text file
+    :return: number of bytes
     """
     with open(filename, 'rb') as file:
         bytes = 0
@@ -26,21 +27,21 @@ def ccwc_chars(filename: str) -> int:
     """
     Count number of chars
 
-    :param textfile: the path to the text file
-    :return: the number of characters in the file
+    :param filename: path to the text file
+    :return: number of characters
     """
-    with open(filename) as file:
-        words = file.read()
+    with open(filename, 'r', encoding='utf-8') as f:
+        text = f.read()
 
-    return len(words)
+    return len(text)
 
 
 def ccwc_lines(filename: str) -> int:
     """
     Count number of lines
 
-    :param textfile: the path to the text file
-    :return: the number of lines in the file
+    :param filename: path to the text file
+    :return: number of lines
     """
     with open(filename) as file:
         lines = file.readlines()
@@ -52,8 +53,8 @@ def ccwc_words(filename: str) -> int:
     """
     Count number of words
 
-    :param textfile: the path to the text file
-    :return: the number of words in the file
+    :param filename: path to the text file
+    :return: number of words
     """
     with open(filename) as file:
         words = file.read()
@@ -61,30 +62,39 @@ def ccwc_words(filename: str) -> int:
     return len(words.split())
 
 
+def count_print(func: Callable, filenames: List[str]) -> None:
+    """
+    :param func: function as corresponding counter
+    :param filenames: list of paths
+    :return: nothing
+    """
+    data = [(func(filename), filename) for filename in filenames]
+
+    if len(data) > 1:
+        total = sum(count for count, _ in data)
+        data.append((total, 'total'))
+
+    max_length = max(len(str(count)) for count, _ in data)
+
+    for count, filename in data:
+        print(f"{count:>{max_length + 2}} {filename}")
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('-c', type=str, nargs='*', help='Count the number of bytes')
-    parser.add_argument('-l', type=str, nargs='*', help='Count the number of lines')
-    parser.add_argument('-m', type=str, nargs='*', help='Count the number of characters')
-    parser.add_argument('-w', type=str, nargs='*', help='Count the number of words')
+    parser.add_argument('-c', type=str, nargs='*', help='Count bytes')
+    parser.add_argument('-l', type=str, nargs='*', help='Count lines')
+    parser.add_argument('-m', type=str, nargs='*', help='Count characters')
+    parser.add_argument('-w', type=str, nargs='*', help='Count words')
 
     args = parser.parse_args()
 
-    if len(args.c) != 1:
-        data = []
-        for filename in args.c:
-            data.append((ccwc_bytes(filename), filename))
-
-        total = 0
-        for item in data:
-            total += item[0]
-
-        data.append((total, 'total'))
-        max_length = max(len(str(item[0])) for item in data)
-
-        for item in data:
-            print(f"{item[0]:>{max_length + 2}} {item[1]}")
-    else:
-        filename = args.c[0]
-        print(f"{ccwc_bytes(filename):>{len(filename)}} {filename}")
+    if args.c:
+        count_print(ccwc_bytes, args.c)
+    elif args.l:
+        count_print(ccwc_lines, args.l)
+    elif args.m:
+        count_print(ccwc_chars, args.m)
+    elif args.w:
+        count_print(ccwc_words, args.w)
