e6247d4291fc68fbcde35bcab2f3def63481e0db
Tony <nya.tony2010@gmail.com>
2024-02-16 12:01:19 +0100

    feat: handle no option provided + reading from standard input cases

--- ccwc.py
+++ ccwc.py
@@ -3,72 +3,84 @@ ccwc - word, line, character, and byte count.
 
 Author: @tonybnya
 """
-from typing import Callable, List
+from typing import Callable, Sequence, Union
 import argparse
+import sys
 
 
-def ccwc_bytes(filename: str) -> int:
+def ccwc_bytes(source: Union[str, None]) -> int:
     """
     Count number of bytes
 
-    :param filename: path to the text file
+    :param source: path to the text file or None for standard input
     :return: number of bytes
     """
-    with open(filename, 'rb') as file:
-        bytes = 0
-
-        for byte in file.read():
-            bytes += 1
+    bytes = 0
+    if source:
+        with open(source, 'rb') as file:
+            for byte in file.read():
+                bytes += 1
+    else:
+        bytes = len(sys.stdin.buffer.read())
 
     return bytes
 
 
-def ccwc_chars(filename: str) -> int:
+def ccwc_chars(source: Union[str, None]) -> int:
     """
     Count number of chars
 
-    :param filename: path to the text file
+    :param source: path to the text file or None for standard input
     :return: number of characters
     """
-    with open(filename, 'r', encoding='utf-8') as f:
-        text = f.read()
+    if source:
+        with open(source, 'r', encoding='utf-8') as file:
+            chars = file.read()
+    else:
+        chars = sys.stdin.read()
 
-    return len(text)
+    return len(chars)
 
 
-def ccwc_lines(filename: str) -> int:
+def ccwc_lines(source: Union[str, None]) -> int:
     """
     Count number of lines
 
-    :param filename: path to the text file
+    :param source: path to the text file or None for standard input
     :return: number of lines
     """
-    with open(filename) as file:
-        lines = file.readlines()
+    if source:
+        with open(source) as file:
+            lines = file.readlines()
+    else:
+        lines = sys.stdin.readlines()
 
     return len(lines)
 
 
-def ccwc_words(filename: str) -> int:
+def ccwc_words(source: Union[str, None]) -> int:
     """
     Count number of words
 
-    :param filename: path to the text file
+    :param source: path to the text file or None for standard input
     :return: number of words
     """
-    with open(filename) as file:
-        words = file.read()
+    if source:
+        with open(source) as file:
+            words = file.read()
+    else:
+        words = sys.stdin.read()
 
     return len(words.split())
 
 
-def count_print(func: Callable, filenames: List[str]) -> None:
+def count_print(func: Callable, sources: Sequence[Union[str, None]]) -> None:
     """
     :param func: function as corresponding counter
-    :param filenames: list of paths
+    :param sources: list of file paths or None for standard input
     :return: nothing
     """
-    data = [(func(filename), filename) for filename in filenames]
+    data = [(func(source), source) for source in sources]
 
     if len(data) > 1:
         total = sum(count for count, _ in data)
@@ -76,11 +88,23 @@ def count_print(func: Callable, filenames: List[str]) -> None:
 
     max_length = max(len(str(count)) for count, _ in data)
 
-    for count, filename in data:
-        print(f"{count:>{max_length + 2}} {filename}")
+    for count, source in data:
+        print(f"{count:>{max_length + 2}} {source}")
 
 
 if __name__ == "__main__":
+    if len(sys.argv) == 2 and not sys.argv[1].startswith('-'):
+        source = sys.argv[1]
+
+        lines = ccwc_lines(source)
+        words = ccwc_words(source)
+        bytes = ccwc_bytes(source)
+
+        max_length = max(len(str(count)) for count in [bytes, lines, words])
+
+        print(f"{lines:>{max_length + 2}}{words:>{max_length + 2}}{bytes:>{max_length + 2}} {source}")
+        sys.exit(0)
+
     parser = argparse.ArgumentParser()
 
     parser.add_argument('-c', type=str, nargs='*', help='Count bytes')
@@ -98,3 +122,12 @@ if __name__ == "__main__":
         count_print(ccwc_chars, args.m)
     elif args.w:
         count_print(ccwc_words, args.w)
+    else:
+        if sys.argv[-1] == '-c':
+            count_print(ccwc_bytes, [''])
+        elif sys.argv[-1] == '-l':
+            count_print(ccwc_lines, [''])
+        elif sys.argv[-1] == '-m':
+            count_print(ccwc_chars, [''])
+        elif sys.argv[-1] == '-w':
+            count_print(ccwc_words, [''])
